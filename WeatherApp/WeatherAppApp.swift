aeb30bc47e609189b2398d943dcca9ca0ac17977
Vaverka123 <maslova.vera.2019@yandex.by>
2024-01-04 15:13:33 +0100

    created project

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Vera Maslava on 4.01.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
